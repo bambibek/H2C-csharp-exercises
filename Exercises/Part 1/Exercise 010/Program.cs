69dadf6500b1919e24722f23773bd3e3b739e2f1
Bibek Bam <bibek2bam@gmail.com>
2020-11-18 00:57:48 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 010/Program.cs
+++ Exercises/Part 1/Exercise 010/Program.cs
@@ -8,8 +8,16 @@ namespace exercise_10
         {
           
 
-           Console.Write(kdfkfj);
-
+           Console.WriteLine("I will tell you a story, but i need some information.");
+            Console.WriteLine("Give the name for main character: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Give the character a profession");
+            string userProfession = Console.ReadLine();
+            Console.WriteLine("Here is your story:");
+            Console.WriteLine("Once upon a time there was a " + userProfession + " called " + userName);
+            Console.WriteLine("On her way to work, " + userName + " often pondered what being " + userProfession + " meant to them.");
+            Console.WriteLine("When you work as s " + userProfession + " you meet interesting people.");
+            Console.WriteLine(userName + " enjoys their work as " + userProfession + ", The End.");
 
 
         }
