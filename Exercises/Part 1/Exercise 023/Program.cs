fdb722ea9777d80d065cd0f22e19b57b8388dd54
Bibek Bam <bibek2bam@gmail.com>
2020-11-19 00:33:43 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 023/Program.cs
+++ Exercises/Part 1/Exercise 023/Program.cs
@@ -7,8 +7,15 @@ namespace exercise_23
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number!");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(first + "+" + second + "=" + (first + second));
+            Console.WriteLine(first + "-" + second + "=" + (first - second));
+            Console.WriteLine(first + "*" + second + "=" + (first * second));
+            Console.WriteLine(first + "/" + second + "=" + (double)first/second);
 
-    }
+        }
   }
 }
