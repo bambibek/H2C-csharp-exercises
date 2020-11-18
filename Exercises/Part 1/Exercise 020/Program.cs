bd0d0af491b6c5adcb34071e802c1c07d201aaa3
Bibek Bam <bibek2bam@gmail.com>
2020-11-18 23:47:16 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 020/Program.cs
+++ Exercises/Part 1/Exercise 020/Program.cs
@@ -7,8 +7,13 @@ namespace exercise_20
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give first number!");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give second number!");
+            int second = Convert.ToInt32(Console.ReadLine());
 
-    }
+            Console.WriteLine(first * second);
+
+        }
   }
 }
