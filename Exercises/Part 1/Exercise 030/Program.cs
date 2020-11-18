dbe4b245167905b189392413a6f9966aed30ba4a
Bibek Bam <bibek2bam@gmail.com>
2020-11-19 01:46:13 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 030/Program.cs
+++ Exercises/Part 1/Exercise 030/Program.cs
@@ -2,12 +2,47 @@
 
 namespace exercise_30
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      // Write your code here:
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give your percent[0 - 100]: ");
+            int percent = Convert.ToInt32(Console.ReadLine());
+            if (percent < 0)
+            {
+                Console.WriteLine("Impossible");
+            }
+            else if (percent <= 49)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (percent <= 59)
+            {
+                Console.WriteLine("Grade : 1");
+            }
+            else if (percent <= 69)
+            {
+                Console.WriteLine("Grade: 2");
+            }
+            else if (percent <= 79)
+            {
+                Console.WriteLine("Grade: 3");
+            }
+            else if (percent <= 89)
+            {
+                Console.WriteLine("Grade: 4");
 
+            }
+            else if (percent <= 100)
+            {
+                Console.WriteLine("Grade: 5");
+
+            }
+            else if (percent > 100)
+            {
+                Console.WriteLine("Outstanding!");
+
+            }
+        }
     }
-  }
 }
