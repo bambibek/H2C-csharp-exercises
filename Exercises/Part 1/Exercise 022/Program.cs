333bcdabafb191fe667c39ec3736c1eff09c81a0
Bibek Bam <bibek2bam@gmail.com>
2020-11-19 00:17:09 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 022/Program.cs
+++ Exercises/Part 1/Exercise 022/Program.cs
@@ -7,8 +7,16 @@ namespace exercise_22
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give the first number!");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number!");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the third number!");
+            int third = Convert.ToInt32(Console.ReadLine());
 
-    }
+            double average = ((first + second + third) / 3.0);
+            Console.WriteLine("The average is: " + average);
+
+        }
   }
 }
