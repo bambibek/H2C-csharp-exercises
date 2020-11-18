5893bbd43c6b753cc93c6dadf094eef8db09fe0b
Bibek Bam <bibek2bam@gmail.com>
2020-11-18 23:16:17 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 015/Program.cs
+++ Exercises/Part 1/Exercise 015/Program.cs
@@ -7,8 +7,24 @@ namespace exercise_15
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            // Write your code here:
+            Console.WriteLine("Give a string");
+            string inputString = Console.ReadLine();
+            Console.WriteLine("Give an integer");
+            string inputInteger = Console.ReadLine();
+            int intValue = Convert.ToInt32(inputInteger);
+            Console.WriteLine("Give a double");
+            string inputDouble = Console.ReadLine();
+            double doubleValue = Convert.ToDouble(inputDouble);
+            Console.WriteLine("Give a boolean");
+            string inputBoolean = Console.ReadLine();
+            bool booleanValue = Convert.ToBoolean(inputBoolean);
+            Console.WriteLine("Your String: " + inputString);
+            Console.WriteLine("Your Integer: " + inputInteger);
+            Console.WriteLine("Your Double: " + inputDouble);
+            Console.WriteLine("Your Boolean: " + inputBoolean);
 
+
+        }
     }
-  }
 }
