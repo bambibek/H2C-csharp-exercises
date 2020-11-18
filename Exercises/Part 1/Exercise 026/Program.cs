6110cf1d7cf610bf97a22b825728fff936fb9320
Bibek Bam <bibek2bam@gmail.com>
2020-11-19 00:48:54 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 026/Program.cs
+++ Exercises/Part 1/Exercise 026/Program.cs
@@ -6,7 +6,12 @@ namespace exercise_26
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 1900)
+            {
+                Console.WriteLine("You're old");
+            }
 
     }
   }
