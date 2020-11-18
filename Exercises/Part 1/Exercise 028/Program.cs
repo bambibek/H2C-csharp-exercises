7e0f3b2e26926c15d516fae5bb256cb89b30606d
Bibek Bam <bibek2bam@gmail.com>
2020-11-19 01:15:16 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 028/Program.cs
+++ Exercises/Part 1/Exercise 028/Program.cs
@@ -6,7 +6,16 @@ namespace exercise_28
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("How old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age >=18)
+            {
+                Console.WriteLine("you're an adult!");
+            }
+            else
+            {
+                Console.WriteLine("you're underage!");
+            }
 
     }
   }
