d9586235af6b254906eae30b8f375a027c3ccfe5
Bibek Bam <bibek2bam@gmail.com>
2020-11-18 23:27:03 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 017/Program.cs
+++ Exercises/Part 1/Exercise 017/Program.cs
@@ -7,8 +7,12 @@ namespace exercise_17
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-
-    }
+            // Write your code here:
+            Console.WriteLine("Input First integer: ");
+            int firstInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input Second integer: ");
+            int secondInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum is: " + (firstInt + secondInt));
+        }
   }
 }
