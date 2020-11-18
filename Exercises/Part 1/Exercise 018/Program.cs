5da140d95c9a74434dc797350f573d45a693a8ee
Bibek Bam <bibek2bam@gmail.com>
2020-11-18 23:31:41 +0200

    18.11.2020

--- Exercises/Part 1/Exercise 018/Program.cs
+++ Exercises/Part 1/Exercise 018/Program.cs
@@ -7,7 +7,14 @@ namespace exercise_18
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give your firsr integer!");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("give your second integer!");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give your third integer!");
+            int third = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Sum is: " + (first + second + third));
+
 
     }
   }
