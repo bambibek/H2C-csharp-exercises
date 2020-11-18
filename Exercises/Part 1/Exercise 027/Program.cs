cb07a9ff39dcfcdb875e27a6dfff9e88faf03ed8
Bibek Bam <bibek2bam@gmail.com>
2020-11-19 00:57:23 +0200

    19.11.2020

--- Exercises/Part 1/Exercise 027/Program.cs
+++ Exercises/Part 1/Exercise 027/Program.cs
@@ -6,8 +6,18 @@ namespace exercise_27
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num > 0)
+            {
+                Console.WriteLine("It is positive");
+                
+            }
+            else
+            {
+                Console.WriteLine("It is negative");
+            }
 
-    }
+        }
   }
 }
