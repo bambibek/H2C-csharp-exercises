7842b3ae368c3e4c2dad2e300403c41cea4ca9fc
Bibek Bam <bibek2bam@gmail.com>
2020-11-21 20:42:15 +0200

    21.11.2020

--- Exercises/Part 1/Exercise 033/Program.cs
+++ Exercises/Part 1/Exercise 033/Program.cs
@@ -6,8 +6,20 @@ namespace exercise_33
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            Console.WriteLine("Give the first string:");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Give the second string:"); 
+            string secondWord = Console.ReadLine();
+            if(firstWord == secondWord)
+            {
+                Console.WriteLine("Echo!");
 
-    }
+            }
+            else
+            {
+                Console.WriteLine("Nope!");
+            }
+
+        }
   }
 }
