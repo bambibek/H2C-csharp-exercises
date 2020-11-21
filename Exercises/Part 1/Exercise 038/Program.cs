8249cc3c806da850f5bc66a6edd2c57a37c7eefc
Bibek Bam <bibek2bam@gmail.com>
2020-11-21 21:40:43 +0200

    21.11.2020

--- Exercises/Part 1/Exercise 038/Program.cs
+++ Exercises/Part 1/Exercise 038/Program.cs
@@ -6,7 +6,26 @@ namespace exercise_38
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("Give a number:  , 0 quits");
+                int inputNum = Convert.ToInt32(Console.ReadLine());
+                if(inputNum == 0)
+                {
+                    break;
+                }
+                else if(inputNum < 0)
+                {
+                    count = count + 1;
+                }
+                else
+                {
+
+                }
+
+            }
+            Console.WriteLine("Total amount of negative numbers: " + count);
 
     }
   }
