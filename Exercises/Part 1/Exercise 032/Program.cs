5b125fb4fc3a5a506404b9dee0621dd8f3c43248
Bibek Bam <bibek2bam@gmail.com>
2020-11-21 20:38:17 +0200

    21.11.2020

--- Exercises/Part 1/Exercise 032/Program.cs
+++ Exercises/Part 1/Exercise 032/Program.cs
@@ -6,7 +6,16 @@ namespace exercise_32
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+           Console.WriteLine("Speak, friend, and enter!");
+           string pass = Console.ReadLine();
+           if(pass == "Mellon")
+            {
+                Console.WriteLine("Welcome, friend.");
+            }
+            else
+            {
+                Console.WriteLine("They've got a cave troll!");
+            }
 
     }
   }
