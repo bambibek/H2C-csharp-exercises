c47331b7b9a819adce858270103cc069ba6dc68c
Bibek Bam <bibek2bam@gmail.com>
2020-11-21 21:46:22 +0200

    21.11.2020

--- Exercises/Part 1/Exercise 039/Program.cs
+++ Exercises/Part 1/Exercise 039/Program.cs
@@ -6,7 +6,23 @@ namespace exercise_39
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("Give a number: ");
+                int numInput = Convert.ToInt32(Console.ReadLine());
+
+                if(numInput == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    count = count + numInput;
+
+                }
+            }
+            Console.WriteLine("Total sum of numbers: " + count);
     }
   }
 }
