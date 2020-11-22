b74d3a46748b9f5e666c79d7da687ce5bac78de3
Bibek Bam <bibek2bam@gmail.com>
2020-11-22 23:14:50 +0200

    22.11.2020

--- Exercises/Part 2/Exercise 044/Program.cs
+++ Exercises/Part 2/Exercise 044/Program.cs
@@ -7,7 +7,23 @@ namespace exercise_44
     public static void Main(string[] args)
     {
 
-      // Write your code here:
+            Console.WriteLine("Give first number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give second Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + " is greater then " + num2);
+            }
+            else if(num1 == num2)
+            {
+                Console.WriteLine(num1 + " is equal to " + num2);
+            }
+            else
+            {
+                Console.WriteLine(num1 + " is less than " + num2);
+            }
 
     }
   }
