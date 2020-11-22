3a43e36992236db3137fc95ef7bc8a8d2eaf1ff6
Bibek Bam <bibek2bam@gmail.com>
2020-11-22 22:53:48 +0200

    22.11.2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -7,8 +7,16 @@ namespace exercise_42
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-    
-    }
+            Console.Write("Give 1st number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Give 2nd number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            //getting squareroot of teh two numbers
+            double squareRoot1 = Math.Sqrt(num1);
+            double squareRoot2 = Math.Sqrt(num2);
+
+            Console.WriteLine("The sum of the square root of the numbers is: " + (squareRoot1 + squareRoot2));
+
+        }
   }
 }
