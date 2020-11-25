bb9728de738990dc7dce48868d2de17a3e6c5b61
Bibek Bam <bibek2bam@gmail.com>
2020-11-25 12:19:46 +0200

    25.11.2020

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -6,8 +6,32 @@ namespace exercise_47
   {
     public static void Main(string[] args)
     {
+            //Section 1
+           /* 
+            int count = 0;
+            Console.WriteLine("Where to?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            while(userInput>=count)
+            {
+                Console.WriteLine(count);
+                count = count + 1;
+            }
+           */
+            
+            
+            //Section 2
+            Console.WriteLine("Where to?");
+            int endCount = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Where from?");
+            int beginCount = Convert.ToInt32(Console.ReadLine());
 
-      // Write your code here:
+            while(beginCount<= endCount)
+            {
+                Console.WriteLine(beginCount);
+
+                beginCount = beginCount + 1;
+            }
+            
       
     }
   }
