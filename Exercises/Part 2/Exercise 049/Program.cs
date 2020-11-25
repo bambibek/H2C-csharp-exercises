9d3f1f28e7988678a96555b3f5698f6b60bdb925
Bibek Bam <bibek2bam@gmail.com>
2020-11-25 21:15:40 +0200

    25.11.2020

--- Exercises/Part 2/Exercise 049/Program.cs
+++ Exercises/Part 2/Exercise 049/Program.cs
@@ -6,13 +6,13 @@ namespace exercise_49
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
- 
-    }
+            PrintPhrase();  // Call your method here:
+        }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
-    }
+    public static void PrintPhrase()
+        {
+            Console.WriteLine("In a hole in the ground there lived a method");
+        }
   }
 }
