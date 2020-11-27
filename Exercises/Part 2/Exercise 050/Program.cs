8f11d3319218b40418c2f40eca04bab930f4f62b
Bibek Bam <bibek2bam@gmail.com>
2020-11-28 00:34:14 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -6,13 +6,23 @@ namespace exercise_50
   {
     public static void Main(string[] args)
     {
-      // Call your method here:
- 
-    }
+
+            Console.WriteLine(" print how many times?");
+            int inputUser = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            // Call your method here:
+            while(count < inputUser)
+            {
+                PrintPhrase();
+                count = count + 1;
+            }
+            
+        }
 
     // Write your method here:
-    public static void PrintPhrase() {
-
+    public static void PrintPhrase()
+    {
+            Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
