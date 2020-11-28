3d246c20e07215322f991ee196bf62dad00cc25c
Bibek Bam <bibek2bam@gmail.com>
2020-11-28 19:34:30 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,39 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+            int sum = 0;
+            int count = 0;
+            int evenCount = 0;
+            int oddCount = 0;
 
-      // Write your code here:
- 
-    }
+            Console.WriteLine("Give a numbers: / -1 to exit");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            while (userInput != -1)
+            {
+                sum = sum + userInput; //sum += userInput
+                count++;
+
+                if(userInput % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+
+          
+                userInput = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Thx! Bye!");
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Numbers:" + count);
+            Console.WriteLine("Average: " + (double)sum/count );
+            Console.WriteLine("Even: " + evenCount);
+            Console.WriteLine("Odd: " + oddCount);
+
+        }
   }
 }
