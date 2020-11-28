f1073f607324e94e8fb221a65adc7347c206b407
Bibek Bam <bibek2bam@gmail.com>
2020-11-28 18:20:35 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 052/Program.cs
+++ Exercises/Part 2/Exercise 052/Program.cs
@@ -6,11 +6,20 @@ namespace exercise_52
   {
     public static void Main(String[] args)
     {
-      // Call your method here:
-
+            // Call your method here:
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            PrintFromNumberToOne(userInput);
     }
 
     // Write your method here:
+    public static void PrintFromNumberToOne(int num)
+        {
+           for(int count= num; count >= 1; count--)
+            {
+                Console.WriteLine(count);
+            }
+
+        }
 
   }
 }
