7174394a6a59b77cc523f69ee3dbc84db4bd0901
Bibek Bam <bibek2bam@gmail.com>
2020-11-28 20:08:58 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 059/Program.cs
+++ Exercises/Part 2/Exercise 059/Program.cs
@@ -11,6 +11,17 @@ namespace exercise_59
     }
 
     // Write your method here:
-
+    public static int Greatest(int n1, int n2, int n3)
+    {
+        if(n1>n2 && n1 > n3)
+            {
+                return n1;
+            }
+        else if(n2>n1 && n2> n3)
+            {
+                return n2;
+            }
+            return n3;
+    }
   }
 }
