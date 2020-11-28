6b02c9de9e91cc03899fa7bef396655ee2fe9bc4
Bibek Bam <bibek2bam@gmail.com>
2020-11-28 17:59:56 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 051/Program.cs
+++ Exercises/Part 2/Exercise 051/Program.cs
@@ -4,14 +4,24 @@ namespace exercise_51
 {
   class Program
   {
+    
     public static void Main(String[] args)
     {
-      // Call your method here:
+            // Call your method here:
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            PrintUnitNumber(userInput);
 
 
     }
 
-    // Write your method here:
+     // Write your method here:
+    public static void PrintUnitNumber(int num)
+    {
+            for (int count = 1; count <= num; count++)
+            {
+                Console.WriteLine(count);
+            }
 
+    }
   }
 }
