864e402532e3a13a449cef0e29480fe1dfc40b1f
Bibek Bam <bibek2bam@gmail.com>
2020-11-28 18:57:59 +0200

    28.11.2020

--- Exercises/Part 2/Exercise 054/Program.cs
+++ Exercises/Part 2/Exercise 054/Program.cs
@@ -6,11 +6,27 @@ namespace exercise_54
   {
     public static void Main(String[] args)
     {
-      // Call your method here:
+            // Call your method here:
+            Console.WriteLine("Give the begining number: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the end number: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
 
-    }
+            DivisibleByThreeInRange(input1, input2);
+        }
 
     // Write your method here:
+    public static void DivisibleByThreeInRange(int start, int end)
+    {
+            for(int count =start; count<=end; count++)
+            {
+                if(count%3 == 0)
+                {
+                    Console.WriteLine(count);
+                }
+                
+            }
+    }
 
   }
 }
