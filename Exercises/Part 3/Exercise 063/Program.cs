fc042d0eb95fe153d1b92015561e988c65a2a309
Bibek Bam <bibek2bam@gmail.com>
2020-12-13 21:44:36 +0200

    13.12.2020

--- Exercises/Part 3/Exercise 063/Program.cs
+++ Exercises/Part 3/Exercise 063/Program.cs
@@ -4,23 +4,36 @@ using System.Collections.Generic;
 
 namespace exercise_63
 {
-  class Program
-  {
-    public static void Main(string[] args)
+    class Program
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == 0)
+        public static void Main(string[] args)
         {
-          break;
+            
+            List<int> list = new List<int>();
+            //1st way
+            /* 
+            while (true)
+             {
+                 int input = Convert.ToInt32(Console.ReadLine());
+                 if (input == 0)
+                 {
+                     break;
+                 }
+
+                 list.Add(input);
+
+             }*/
+
+            //2nd way
+            int input = Convert.ToInt32(Console.ReadLine());
+           while(input!=0)
+            {
+                list.Add(input);
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(list[1] + list[2]);
+
         }
-
-        list.Add(input);
-      }
-      Console.WriteLine(list[0]);
     }
-
-  }
 }
