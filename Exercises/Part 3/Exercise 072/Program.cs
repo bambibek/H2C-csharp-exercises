77c21697676cea5d8fa96db7e33115d36f212d6f
Bibek Bam <bibek2bam@gmail.com>
2020-12-16 00:22:24 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 072/Program.cs
+++ Exercises/Part 3/Exercise 072/Program.cs
@@ -7,7 +7,34 @@ namespace exercise_72
   {
     public static void Main(string[] args)
     {
+            Console.WriteLine(" Give numbers: / give 9999 to stop");
+            int input = Convert.ToInt32(Console.ReadLine());
 
+            List <int> list = new List<int>();
+            while(input != 9999)
+            {
+                list.Add(input);
+                input = Convert.ToInt32(Console.ReadLine());
+            }
+            int small = list[0];
+            foreach(int smallest in list)
+            {
+                if (small >=smallest)
+                    {
+                    small = smallest;
+                }
+            }
+
+            Console.WriteLine(small);
+
+            for(int i =0; i< list.Count; i++)
+            {
+                int smallestNum = list[i];
+                if(small == smallestNum)
+                {
+                    Console.WriteLine(small + " is at index " + i);
+                }
+            }
     }
   }
 }
