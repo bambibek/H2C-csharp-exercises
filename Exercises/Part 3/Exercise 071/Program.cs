68e88f76fd4b7f6a2ccf37477aa767ad9742b839
Bibek Bam <bibek2bam@gmail.com>
2020-12-15 15:32:28 +0200

    15.12.2020

--- Exercises/Part 3/Exercise 071/Program.cs
+++ Exercises/Part 3/Exercise 071/Program.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace exercise_71
 {
   class Program
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
-      {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
-        {
-          break;
-        }
-        list.Add(input);
-      }
+            List<int> list = new List<int>();
+            int input = Convert.ToInt32(Console.ReadLine());
+            while (input != -1)
+            {
 
+              list.Add(input);
+
+              input = Convert.ToInt32(Console.ReadLine());
+             }
+
+            Console.WriteLine("Search For?");
+            int search = Convert.ToInt32(Console.ReadLine());
+            int index = list.IndexOf(search);
+            
+                for (int i = 0; i < list.Count; i++)
+                {
+                    int num = list[i];
+                    if(search==num)
+                    {
+                        Console.WriteLine(search + "is at index " + i);
+                    }
+                }
+
+        }
     }
-  }
 }
