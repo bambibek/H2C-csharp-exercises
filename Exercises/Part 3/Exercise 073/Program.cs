701ab352991a5978bcf1e73a282b5f1f49aa2f08
Bibek Bam <bibek2bam@gmail.com>
2020-12-16 01:55:02 +0200

    16.12.2020

--- Exercises/Part 3/Exercise 073/Program.cs
+++ Exercises/Part 3/Exercise 073/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_73
 {
@@ -7,17 +8,36 @@ namespace exercise_73
   {
     public static void Main(string[] args)
     {
-      List<int> list = new List<int>();
-      while (true)
+    int input = Convert.ToInt32(Console.ReadLine());
+    List<int> list = new List<int>();
+      while (input != -1)
       {
-        int input = Convert.ToInt32(Console.ReadLine());
-        if (input == -1)
-        {
-          break;
-        }
         list.Add(input);
+        input = Convert.ToInt32(Console.ReadLine());
       }
 
+            //using teh Linq (one way of getting the sum)
+            /*
+                  int index = list.IndexOf(input);
+
+                  int sum = list.Sum();
+
+                  Console.WriteLine("Sum: " + sum);
+                  */
+
+            //using foreacha loop (another way of getting the sum)
+            int sum = 0;
+            foreach (int values in list)  // getting all values in the list
+                                          //Console.WriteLine(values); //prints the values
+
+                sum += values; // also written as sum = sum+value;
+            Console.WriteLine("Sum: " + sum);
+
+
+
+
+
+
+        }
     }
-  }
 }
