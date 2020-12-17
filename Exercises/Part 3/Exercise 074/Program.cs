cb4dd44a1b7f3b6233bf7205356a2b2e59ab03e9
Bibek Bam <bibek2bam@gmail.com>
2020-12-17 11:46:06 +0200

    17.12.2020

--- Exercises/Part 3/Exercise 074/Program.cs
+++ Exercises/Part 3/Exercise 074/Program.cs
@@ -8,15 +8,29 @@ namespace exercise_74
     public static void Main(string[] args)
     {
       List<string> list = new List<string>();
-      while (true)
+      string input = Console.ReadLine();
+      while (input!="")
       {
-        string input = Console.ReadLine();
-        if (input == "")
-        {
-          break;
-        }
         list.Add(input);
+        input = Console.ReadLine();
       }
+
+            Console.WriteLine("Search For?");
+            string search =Console.ReadLine();
+            
+            foreach(string value in list)
+            {
+                
+                if (search == value)
+                {
+                    Console.WriteLine(value + " was found!");
+                    break;
+                }
+                else
+                    Console.WriteLine(search + " was not found!");
+                break;
+            }
+            
       
     }
   }
