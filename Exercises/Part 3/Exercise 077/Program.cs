e8bd10172d7b5738478a915b92910fbe128c52ce
Bibek Bam <bibek2bam@gmail.com>
2020-12-17 18:51:36 +0200

    17.12.2020

--- Exercises/Part 3/Exercise 077/Program.cs
+++ Exercises/Part 3/Exercise 077/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace exercise_77
 {
@@ -7,9 +8,30 @@ namespace exercise_77
   {
     public static void Main(string[] args)
     {
-      // You can try your method here if you want
-    }
+            // You can try your method here if you want
+            List<string> names = new List<string>();
+            names.Add("First");
+            names.Add("Second");
+            names.Add("Third");
 
+            //Console.WriteLine( names.ElementAt(2));
+            names.ForEach(Console.WriteLine);
+
+            RemoveLast(names);
+            RemoveLast(names);
+
+            names.ForEach(Console.WriteLine);
+        }
+
+        public static void RemoveLast(List<string> nameList)
+        {
+            int size = nameList.Count();
+
+            int lastElementIndex = size - 1;
+
+            nameList.RemoveAt(lastElementIndex);
+            
+        }
   }
 }
 
