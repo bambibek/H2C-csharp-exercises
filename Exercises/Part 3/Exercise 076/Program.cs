a51650dad30f30a343f57b28fda017d8cc405264
Bibek Bam <bibek2bam@gmail.com>
2020-12-17 18:16:07 +0200

    17.12.2020

--- Exercises/Part 3/Exercise 076/Program.cs
+++ Exercises/Part 3/Exercise 076/Program.cs
@@ -8,7 +8,27 @@ namespace exercise_76
     public static void Main(string[] args)
     {
 
-    }
+            List<int> numbers = new List<int>();
+            numbers.Add(2);
+            numbers.Add(4);
+            numbers.Add(3);
+            numbers.Add(6);
+            numbers.Add(5);
+            numbers.Add(8);
+
+            int finalSum = Sum(numbers);
+            Console.WriteLine(finalSum);
+            
+        }
+    public static int Sum(List <int> numbers)
+        {
+            int sum = 0;
+            foreach(int values in numbers)
+            {
+                sum += values;
+            }
+            return sum;
+        }
 
   }
 }
